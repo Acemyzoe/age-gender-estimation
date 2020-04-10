afff348dd6d4caa5847493ce2dace8d70432759e
ace <acemyzoe@outlook.com>
2020-04-10 14:51:39 +0800

    opt

--- train.py
+++ train.py
@@ -14,6 +14,9 @@ from random_eraser import get_random_eraser
 
 logging.basicConfig(level=logging.DEBUG)
 
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 def get_args():
     parser = argparse.ArgumentParser(description="This script trains the CNN model for age and gender estimation.",
